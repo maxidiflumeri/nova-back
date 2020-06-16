b7838443616876c6235be57b52ff21f209f8e618
Ezequiel Laboranti <ezelaboranti@hotmail.com>
2020-06-16 18:40:57 -0300

    Se agregan los post sin validación

--- src/marca/DAO.js
+++ src/marca/DAO.js
@@ -1,25 +1,65 @@
 import crearConexion from '../../db/conexionDB.js'
+import Joi from '@hapi/joi'
 //import sql from "mssql"
 //DEV BY EZE LABORANTI
 
 const tabla = 'MARCAS'
 
-async function obtenerTodos() {  
+async function obtenerTodos() {
     const conn = crearConexion()
     let lista = []
-    try{        
+    try {
         lista = await conn.select().from(tabla)
         conn.destroy()
     }
-    catch(error){
+    catch (error) {
         console.log(error)
         conn.destroy()
     }
-    return lista    
+    return lista
 }
 
+async function agregar(objeto) {
+    const conn = crearConexion()
+    let resultado = null
+    if (validar(objeto)) {
+        try {
+            await conn.insert(objeto).into(tabla)
+            resultado = {
+                "msg": "Objeto agregado."
+            }
+            conn.destroy()
+        }
+        catch (error) {
+            console.log(error)
+            conn.destroy()
+        }
+        console.log(resultado)
+    } else {
+        resultado = {
+            "error": 400,
+            "msg": "Body Incorrecto."
+        }
+    }
 
+    return resultado
+}
 
-export default{
-    obtenerTodos
+function validar(objeto) {
+    const objetoSchema = {
+        descripcion: Joi.string().required()
+    }
+
+    const { error } = Joi.validate(objeto, objetoSchema)
+    if (error) {
+        console.log('Error validate')
+        return false
+    }
+    console.log('Correcto validate')
+    return true
+}
+
+export default {
+    obtenerTodos,
+    agregar
 }
