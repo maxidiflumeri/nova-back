b9bb2accec9ab18a98bcd4d2f224119c930180c6
maxidiflumeri <maxidiflumeri@gmail.com>
2020-07-04 21:00:16 -0300

    .

--- DB/conexionDB.js
+++ DB/conexionDB.js
@@ -29,23 +29,6 @@ function crearConexion(){
     return knex
 }
 
-// function crearConexion(){
-//     const knex = knexLib({
-//         client: 'mssql',
-//         connection: {
-//             server: "srvnova.database.windows.net", 
-//             database: "nova-db",
-//             user: "nova", 
-//             password: "Nov42020",
-//         }, 
-//         options: {
-//             port: 1433,
-//             encrypt: true,
-//             enableArithAbort: true
-//         }
-//     })
-//     return knex
-// }
 
 export default getConexion
 
